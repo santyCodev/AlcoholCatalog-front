ce54f44ca5d6745c8fb6e9142426fb0cdbfd2593
Santy <santi.codev@gmail.com>
2021-10-28 14:24:39 +0200

    DEV: Maquetacion brand list y componente brand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrandListComponent } from './brand-list/brand-list.component';
+import { BrandComponent } from './brand/brand.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrandListComponent
+    BrandListComponent,
+    BrandComponent
   ],
   imports: [
     BrowserModule,

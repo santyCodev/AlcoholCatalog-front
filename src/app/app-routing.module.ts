b6fb00ccf14b472322f3c213a27c58fcbc38107c
Santy <santi.codev@gmail.com>
2021-10-28 13:08:11 +0200

    DEV: Brand list component and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandListComponent } from './brand-list/brand-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'brandList', pathMatch: 'full'},
+  { path: 'brandList', component: BrandListComponent,  pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrandListComponent } from './brand-list/brand-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandListComponent
   ],
   imports: [
     BrowserModule,
